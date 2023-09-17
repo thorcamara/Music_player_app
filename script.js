994f7fefffe8c9ba5526c205465c4b5d844e88f0
thorcamara <thorcamaraalthoff@gmail.com>
2023-09-17 18:56:10 -0300

    Added function updateProgressBar, setProgressBar and EventListeners

--- script.js
+++ script.js
@@ -71,3 +71,28 @@ function changeMusic(direction) {
   loadMusic(songs[musicIndex]);
   playMusic();
 }
+
+function updateProgressBar() {
+  const { duration, currentTime } = music;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+
+  const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
+  durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
+  currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
+}
+
+function setProgressBar(e) {
+  const width = playerProgress.clientWidth;
+  const clickX = e.offsetX;
+  music.currentTime = (clickX / width) * music.duration;
+}
+
+playBtn.addEventListener('click', togglePlay);
+prevBtn.addEventListener('click', () => changeMusic(-1));
+nextBtn.addEventListener('click', () => changeMusic(1));
+music.addEventListener('ended', () => changeMusic(1));
+music.addEventListener('timeupdate', updateProgressBar);
+playerProgress.addEventListener('click', setProgressBar);
+
+loadMusic(songs[musicIndex]);
